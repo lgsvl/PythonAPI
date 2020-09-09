3e007c9dd3185bbd5e4f6c409368249976c167c4
Steve Lemke <steve.lemke@lge.com>
2020-09-09 01:54:56 -0700

    AUTO-2854: Fix hardcoded bridge address

--- quickstart/22-connecting-bridge.py
+++ quickstart/22-connecting-bridge.py
@@ -19,19 +19,19 @@ spawns = sim.get_spawn()
 
 state = lgsvl.AgentState()
 state.transform = spawns[0]
-a = sim.add_agent("Lincoln2017MKZ (Apollo 5.0)", lgsvl.AgentType.EGO, state)
+ego = sim.add_agent("Lincoln2017MKZ (Apollo 5.0)", lgsvl.AgentType.EGO, state)
 
 # An EGO will not connect to a bridge unless commanded to
-print("Bridge connected:", a.bridge_connected)
+print("Bridge connected:", ego.bridge_connected)
 
 # The EGO is now looking for a bridge at the specified IP and port
-a.connect_bridge("10.195.248.183", 9090)
+ego.connect_bridge(os.environ.get("AUTOPILOT_HOST", "127.0.0.1"), 9090)
 
 print("Waiting for connection...")
 
-while not a.bridge_connected:
+while not ego.bridge_connected:
     time.sleep(1)
 
-print("Bridge connected:", a.bridge_connected)
+print("Bridge connected:", ego.bridge_connected)
 
 sim.run()
